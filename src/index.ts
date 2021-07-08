1297730faa18736f17cd111cb79f6241061bf502
Mauro Oliveira <mauro.oliveira@agilecontent.com>
2021-07-08 00:04:46 -0300

    add Eslint and Prettier

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import {EC2} from 'aws-sdk';
-import {describeInstances} from './async-executor';
-import {getInstances} from './response-handler';
-import {print} from './report';
+import { EC2 } from 'aws-sdk';
+import { describeInstances } from './async-executor';
+import { getInstances } from './response-handler';
+import { print } from './report';
 
-const ec2 = new EC2({apiVersion: '2016-11-15', region: 'us-east-1'});
+const ec2 = new EC2({ apiVersion: '2016-11-15', region: 'us-east-1' });
 
 const run = async () => {
-    const response = await describeInstances(ec2);
-    const instances = getInstances(response);
-    print(instances);
-}
+  const response = await describeInstances(ec2);
+  const instances = getInstances(response);
+  print(instances);
+};
 
 run();
--- src/response-handler.ts
+++ src/response-handler.ts
@@ -3,9 +3,9 @@ import { DescribeInstancesResult, InstanceList } from 'aws-sdk/clients/ec2';
 export function getInstances(param: DescribeInstancesResult): InstanceList {
   const instances: InstanceList = [];
 
-  param.Reservations?.forEach(res => {
-      res.Instances?.forEach(inst => instances.push(inst));
+  param.Reservations?.forEach((res) => {
+    res.Instances?.forEach((inst) => instances.push(inst));
   });
 
   return instances;
-};
+}
