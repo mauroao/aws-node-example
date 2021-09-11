3d7b91b695dc37e01d087fb0af390ca6b232939d
Mauro Oliveira <mauro.oliveira@agilecontent.com>
2021-09-11 18:47:49 -0300

    add WeekDayTime and WeekEndTime fields

--- src/report.ts
+++ src/report.ts
@@ -1,20 +1,30 @@
 import { InstanceList, Instance } from 'aws-sdk/clients/ec2';
 import chalk from 'chalk';
 
+function getTagValue(instance: Instance, key: string): string {
+  return instance.Tags?.find((tag) => tag.Key === key)?.Value || '';
+}
+
 class ReportLine {
   InstanceId: string;
   State: string;
   Platform: string;
   Name: string;
   MonitorType: string;
+  WeekDayTime: string;
+  WeekEndTime: string;
 
   constructor(instance: Instance) {
     this.InstanceId = instance.InstanceId || '';
     this.State = instance.State?.Name || '';
     this.Platform = instance.Platform || 'linux';
     this.Name = instance.Tags?.find((tag) => tag.Key === 'Name')?.Value || '';
-    this.MonitorType =
-      instance.Tags?.find((tag) => tag.Key === 'MonitorType')?.Value || '';
+    this.MonitorType = getTagValue(instance, 'MonitorType');
+
+    const monitorSettings = getTagValue(instance, 'MonitorSettings') || '{}';
+
+    this.WeekDayTime = JSON.parse(monitorSettings).WeekDayTime || '';
+    this.WeekEndTime = JSON.parse(monitorSettings).WeekEndTime || '';
   }
 }
 
@@ -28,11 +38,12 @@ export function print(instances: InstanceList): void {
   });
 
   items.forEach((item) => {
-    //const instanceId = item.InstanceId;
     const name = item.Name.padEnd(34, ' ');
     let state = item.State?.padEnd(10, ' ');
     const platform = item.Platform.padEnd(9, ' ');
-    const monitorType = item.MonitorType;
+    const monitorType = item.MonitorType.padEnd(16, ' ');
+    const weekDayTime = item.WeekDayTime.padEnd(21, ' ');
+    const weekEndTime = item.WeekEndTime.padEnd(10, ' ');
 
     if (item.State == 'stopped') {
       state = chalk.red(state);
@@ -41,6 +52,6 @@ export function print(instances: InstanceList): void {
       state = chalk.green(state);
     }
 
-    console.log(/*instanceId, */ name, state, platform, monitorType);
+    console.log(name, state, platform, monitorType, weekDayTime, weekEndTime);
   });
 }
